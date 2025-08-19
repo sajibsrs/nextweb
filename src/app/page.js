a8fd44c5be62090c182ca070e6cd1e1703f5f48c
sajibsrs <sajibsrs@gmail.com>
2025-08-20 04:18:27 +0600

    updated home page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
+import { SignupForm } from "@/ui/signup-form";
 import styles from "./page.module.css";
 import Footer from "@/ui/footer";
 
 export default function Home() {
   return (
     <>
+      <section className={styles.title}>
+        <h2>Home</h2>
+        <hr />
+      </section>
       <main className={styles.main}>
-        <h1>Home</h1>
+        <SignupForm />
       </main>
       <Footer />
     </>
